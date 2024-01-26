b8c1dabcef7a290eec338bc61250303632cff007
Eeshvar Das (Erik Douglas Ward) <contactme@eeshvardasikcm.com>
2024-01-26 10:26:05 -0800

    added quit option to menu

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # khan_academy_math_study_notes (beta) 0.2.0
 <p>This C++ repository has some code I wrote during my studies of the Khan Academy Class 10(OD) course.
-Some of the material covered in this course will certainly help my C++ templates, and also help my [EeshvarDasIKCM profile score higher on Codingame](https://www.codingame.com/profile/70a154e089698866b393f458447036523367585).
+Some of the material covered in this course will certainly help my C++ templates, and also help me score higher on my <a href="https://www.codingame.com/profile/70a154e089698866b393f458447036523367585">EeshvarDasIKCM Codingame profile</a>.
 This C++ CMake project uses C++17, and should not include any code from C++20 or newer for some time.</p>
 
 ## eeshvardasikcm (EeshvarDasIKCM)
@@ -10,3 +10,7 @@ This course is extremely interesting to me.</p>
 <p>I scored a perfect 4.0 in first year Calculus in college, but then I stopped right there.
 It will be nice to catch up on some geometry and trigonometry during this course.
 Finishing my BS in Computer Science would be a major accomplishment for me and I am excited that Khan Academy offers this Class 10(OD) course in mathematics.</p>
+
+## command line menu <b>enhancement</b>
+- add a quit option to allow menu to repeat again after one selection.
+- maybe more...
--- main.cpp
+++ main.cpp
@@ -1,230 +1,194 @@
 #include <iostream>
 #include <cmath>
+#include <utility>
 #include <vector>
+#include <cstdint>
+
 #ifdef VARIABLE_INCLUDE_standard
 #include "standard.h"
 #endif
 #ifdef VARIABLE_INLUCDE_experimental
 #include "experimental.h"
 #endif
-using namespace std;
-namespace class_10_od {
-    struct _menu_control {
-        string name;
-        static const int EXIT_PROGRAM;
-        int control=EXIT_PROGRAM;
-    };const int _menu_control::EXIT_PROGRAM=EXIT_SUCCESS;
-    struct _menu_runnable : _menu_control {
+namespace class10Od {
+    struct menuControl {
+    };
+
+    struct menuRunnable : menuControl {
         virtual void inputVariables() {};
         void runFromMenu() {
             inputVariables();
         };
     };
-
-    struct _point {
-        float x, y;
-        [[nodiscard]] string printV() const {
-            string s="(";
-            s.append(to_string(x));
-            s.append(", ");
-            s.append(to_string(y));
-            s.append(") ");
-            return s;
-        }
+    struct menuItemType {
+    public:
+        static const std::uint8_t controlType = 101U;
+        static const std::uint8_t runnableType = 102U;
+    };
+    struct point {
+        float x;
+        float y;
     };
 
-    struct _line {
-        _point A, B;
+    struct line {
     };
 
-    struct _unit_of_measure {
-        static const string _km;
-        static const string _hr;
-        static string _km_per_h() {
-            string s=" ";
-            s.append(_km);
-            s.append("/");
-            s.append(_unit_of_measure::_hr);
-            s.append(" ");
+    struct unitOfMeasure {
+        static const std::string km;
+        static const std::string hr;
+        static std::string kmPerH() {
+            std::string s=std::string(" ")
+                    .append(km)
+                    .append("/")
+                    .append(unitOfMeasure::hr)
+                    .append(" ");
             return s;
         };
     };
-    const string _unit_of_measure::_km = "km";
-    const string _unit_of_measure::_hr = "hour";
+    const std::string unitOfMeasure::km = "km";
+    const std::string unitOfMeasure::hr = "hour";
 
-    struct _speed : _menu_runnable {
-        float v=0;
+    struct speed : menuRunnable {private:
+        float v=static_cast<float>(0);
         void calculate(float kilometers, float hours) {
             v = kilometers / hours;
         };
-        [[nodiscard]] string getV() const {
-            string s=to_string(v);
-            s.append(_unit_of_measure::_km_per_h());
+        [[nodiscard]] std::string getV() const {
+            std::string s=std::to_string(v)
+                    .append(unitOfMeasure::kmPerH());
             return s;
         };
-        void inputVariables() override {
-            cout << "distance(" << _unit_of_measure::_km << "): ";
+        void inputVariables() override  {
+            std::cout << "distance(" << unitOfMeasure::km << "): ";
             float d;
-            cin >> d; cin.ignore();
-            cout << "time(" << _unit_of_measure::_hr << "): ";
+            std::cin >> d; (void)std::cin.ignore();
+            std::cout << "time(" << unitOfMeasure::hr << "): ";
             float t;
-            cin >> t; cin.ignore();
+            std::cin >> t; (void)std::cin.ignore();
             calculate(d,t);
-            cout << getV() << endl;
+            std::cout << getV() << std::endl;
         }
     };
 
-    struct _line_formula {
-        float Ax, Ay, Bx, By;
+    struct lineFormula {
 #ifdef VARIABLE_INLUCDE_experimental
-        _point _equation_1(float x, float y, float xn=11, float yn=65) {
-            return _point((float _f1(*(x)))*(xn), (float _f2(*(y)))*(yn));
+        point _equation_1(float x, float y, float xn=11, float yn=65) {
+            return point((float _f1(*(x)))*(xn), (float _f2(*(y)))*(yn));
         }
 
-        _point intersect(_line l1, _line_formula l2) {
+        point intersect(line l1, lineFormula l2) {
 
         }
 #endif
     };
 
-    struct _sin : _menu_runnable {
-        static float nx(float n, float x) {
+    struct trigSin : menuRunnable {
+        static double nx(double n, double x) {
             return sin(n*x);
         }
         void inputVariables() override {
-            cout << "sin _n*x" << endl;
-            cout << "_n: ";
+            std::cout << "sin n*x" << std::endl;
+            std::cout << "n: ";
             float n;
-            cin >> n; cin.ignore();
-            cout << "x: ";
+            std::cin >> n; (void)std::cin.ignore();
+            std::cout << "x: ";
             float x;
-            cin >> x; cin.ignore();
-            cout << "sin " << n << "x, ";
-            cout << "where x=" << x << " is equal to: ";
-            cout << nx(n,x) << endl;
+            std::cin >> x; (void)std::cin.ignore();
+            std::cout << "sin " << n << "x, ";
+            std::cout << "where x=" << x << " is equal to: ";
+            std::cout << nx(n,x) << std::endl;
         };
     };
 
-    struct _menu {
+    struct menu {
     private:
-        struct _item {
-            _item(nullptr_t pVoid) {
-
-            }
-
+        struct menuItem {
         private:
-            _menu_control* pMc;
+            int n;
+            std::string name;
+            menuControl* pMc;
+            menuRunnable* pMr;
         public:
-            _menu_runnable* pMr;
-            explicit _item(_menu_runnable* pMenuRunnable):pMr(pMenuRunnable) {
+            static int totalCreated;
+            explicit menuItem(std::string name, menuRunnable* pMenuRunnable):
+            pMr(pMenuRunnable), name(name) {
                pMc=nullptr;
+               totalCreated++;
+               n=totalCreated;
             };
-            explicit _item(_menu_control* pMenuControl):pMc(pMenuControl){
+            explicit menuItem(std::string name, menuControl* pMenuControl):
+            pMc(pMenuControl), name(name) {
                 pMr= nullptr;
+                totalCreated++;
+                n=totalCreated;
             };
-            [[nodiscard]] _menu_control * getPMc() const {return pMc;}
+            [[nodiscard]] menuControl * getPMc() const {return pMc;}
+            [[nodiscard]] menuRunnable * getPMr() const {return pMr;}
+            [[nodiscard]] int getN() const {return n;}
+            [[nodiscard]] std::string getName() {return name;}
         };
-        vector<_item> menuItems;
-        struct _n_menu_item_showing {
+        std::vector<menuItem> menuItems;
+        struct indexedShownMenuItem {
+        private:
+            menuItem* pItem;
         public:
-            int _n;
-            _item item = _item(nullptr);
-            _n_menu_item_showing(int n, _menu_control* pMc): _n(n) {
-                item=_item(pMc);
-            };
-        };
-        vector<_n_menu_item_showing> nMenuItemsShowing;
-        void _printItem(
-                _menu_runnable* pMenuRunnable=nullptr,
-                _menu_control* pMenuControl=nullptr) {
-            if (nullptr==pMenuRunnable) {
-                if (nullptr==pMenuControl) {
-                    throw logic_error("Only one object pointer can be nullptr. ");
-                } else {
-                    bool contains=false;
-                    int n;
-                    for (_item mc : menuItems) {
-                        if (mc.getPMc()==pMenuControl) {
-                            contains=true;
-
-                        }
-                    }
-                    if (contains) {
-
+            [[nodiscard]] int getN() const {
+                int retN;
+                if (pItem->getPMr()== nullptr) {
+                    if (pItem->getPMc()== nullptr) {
+                        throw std::logic_error("indexed menu item not built properly.");
                     } else {
-                        nMenuItemsShowing.emplace_back(_n_menu_item_showing(menuItems.size() + 1, pMenuControl));
+                        retN = pItem->getN();
                     }
-                    cout << *pI << ") ";
-                    cout << pMenuControl->name << endl;
-                }
-            } else {
-                bool contains=false;
-                for (_item mr : menuItems) {
-                    if (mr.pMr==pMenuRunnable) {
-                        contains=true;
-                    }
-                }
-                if (contains) {
-
                 } else {
-                    nMenuItemsShowing.emplace_back(_n_menu_item_showing{*pI, _item(pMenuRunnable)});
+                    retN = pItem->getN();
                 }
-                cout << *pI << ") ";
-                cout << pMenuRunnable->name << endl;
+                return retN;
+            }
+            [[nodiscard]] menuItem* getPItem() const{return pItem;}
+        };
+        std::vector<menuItem> nMenuItemsShowing;
+        static void drawItemOnMenu(menuItem shownMenuItem) {
+            int n = shownMenuItem.getN();
+            std::cout << n << ") ";
+            std::cout << shownMenuItem.getName() << std::endl;
+        }
+    public:
+        void addExitItem() {
+            menuControl mc;
+            menuItem newItem("exit", &mc);
+            menuItem shownMenuItem = nMenuItemsShowing.emplace_back(newItem);
+            if (shownMenuItem.getPMc() != &mc) {
+                throw std::logic_error("control style menu item was "
+                                       "not added to the list of"
+                                       "created menu items properly.");
             }
         }
-        void _printItem(_menu_control* pMenuControl) {
-            _printItem(nullptr, pMenuControl);
-        }
-        void showItem(_menu_runnable* pMenuRunnable) {
-            _printItem(pMenuRunnable);
-        }
-        void showItem(_menu_control* pMenuControl) {
-            _printItem(pMenuControl);
-        }
-        using _exit_program = _menu_control;
-    public:
-        void add_exit_item() {
-            _exit_program menuControl;
-            menuControl.control=_menu_control::EXIT_PROGRAM;
-            menuControl.name="exit";
-            menuItems.emplace_back(_item(&menuControl));
-        }
-        void add_item(
-                _menu_runnable* pMenuRunnable,
-                const string& name,
-                int _control_option = _menu_control::EXIT_PROGRAM) {
-            pMenuRunnable->control=_control_option;
-            pMenuRunnable->name=name;
-            menuItems.emplace_back(_item(pMenuRunnable));
+        void addItem(menuRunnable *pMenuRunnable, const std::string &name) {
+            menuItem newItem(name, pMenuRunnable);
+            menuItem shownMenuItem = nMenuItemsShowing.emplace_back(newItem);
+            if (shownMenuItem.getPMr()!=pMenuRunnable) {
+                throw std::logic_error("runnable style menu item was "
+                                       "not added to the list of"
+                                       "created menu items properly.");
+            }
         }
         void show() {
             bool continueRunning = true;
             while (continueRunning) {
-                cout << endl;
-                for (const _item &one_menu_item: menuItems) {
-                    if (nullptr==one_menu_item.pMr) {
-                        showItem(one_menu_item.getPMc());
-                    } else {
-                        showItem(one_menu_item.pMr);
-                    }
+                std::cout << std::endl;
+                for (menuItem oneMenuItem: nMenuItemsShowing) {
+                    drawItemOnMenu(oneMenuItem);
                 }
-                for (const _n_menu_item_showing &one_menu_n_item: nMenuItemsShowing) {
-                    if (nullptr==one_menu_n_item.item.pMr) {
-                        showItem(one_menu_n_item);
-                    } else {
-                        showItem(one_menu_n_item);
-                    }
-                }
-                cout << "Class 10(OD) menu: ";
+                std::cout << "Class 10(OD) menu: ";
                 int selection;
-                cin >> selection; cin.ignore();
-                for (_n_menu_item_showing nI: nMenuItemsShowing) {
-                    if (nI._n == selection) {
-                        if (nullptr == nI.item.pMr) {
+                std::cin >> selection; (void)std::cin.ignore();
+                for (menuItem nI: nMenuItemsShowing) {
+                    if (nI.getN() == selection) {
+                        if (nullptr == nI.getPMr()) {
                             continueRunning = false;
                         } else {
-                            nI.item.pMr->runFromMenu();
+                            nI.getPMr()->runFromMenu();
                             continueRunning = true;
                         }
                     }
@@ -233,15 +197,16 @@ namespace class_10_od {
         };
     };
 }
-using namespace class_10_od;
+int class10Od::menu::menuItem::totalCreated=0;
+
 int main() {
     std::cout << "khan_academy_math_study_notes (beta)" << std::endl;
-    class_10_od::_speed velocity;
-    class_10_od::_sin trigSin;
-    class_10_od::_menu menu;
-    menu.add_item(&velocity, "velocity");
-    menu.add_item(&trigSin, "trigonometry sin");
-    menu.add_exit_item();
+    class10Od::speed velocity;
+    class10Od::trigSin trigSin;
+    class10Od::menu menu;
+    menu.addItem(&velocity, "velocity");
+    menu.addItem(&trigSin, "trigonometry sin");
+    menu.addExitItem();
     menu.show();
     return 0;
 }
