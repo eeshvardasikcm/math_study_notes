b9ffa8405458e0a3110dc13a0427c0c70d0de888
Eeshvar Das (Erik Douglas Ward) <contactme@eeshvardasikcm.com>
2024-01-26 02:31:12 -0800

    added velocity

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# khan_academy_math_study_notes (beta) 0.0.0.3
+# khan_academy_math_study_notes (beta) 0.0.0.4
 <p>This C++ repository has some code I wrote during my studies of the Khan Academy Class 10(OD) course.
 Some of the material covered in this course will certainly help my C++ templates, and also help my [EeshvarDasIKCM profile score higher on Codingame](https://www.codingame.com/profile/70a154e089698866b393f458447036523367585).
 This C++ CMake project uses C++17, and should not include any code from C++20 or newer for some time.</p>
+
 ## eeshvardasikcm (EeshvarDasIKCM)
 <p>While restarting Khan Academy again today, it became obvious to me that I want to learn the Class 10(OD) mathematics course.
 Before having time to look up exactly what a Class 10(OD) is, naturally I started with the course challenge.
--- main.cpp
+++ main.cpp
@@ -9,19 +9,51 @@ using namespace std;
 namespace class_10_od {
     struct _point {
         float x, y;
-
-        string printV();
+        [[nodiscard]] string printV() const {
+            string s="(";
+            s.append(to_string(x));
+            s.append(", ");
+            s.append(to_string(y));
+            s.append(") ");
+            return s;
+        }
     };
 
     struct _line {
         _point A, B;
     };
 
+    struct _unit_of_measure {
+        static const string _km;
+        static const string _hr;
+        static string _km_per_h() {
+            string s=_km;
+            s.append("/");
+            s.append(_unit_of_measure::_hr);
+            s.append(" ");
+            return s;
+        };
+    };
+    const string _unit_of_measure::_km = "km";
+    const string _unit_of_measure::_hr = "hours";
+
+    struct _speed {
+        float v;
+        void calculate(float kilometers, float hours) {
+            v = kilometers / hours;
+        };
+        [[nodiscard]] string printV() const {
+            string s=to_string(v);
+            s.append(_unit_of_measure::_km_per_h());
+            return s;
+        };
+    };
+
     struct _line_formula {
-        float y, x;
+        float Ax, Ay, Bx, By;
 #ifdef VARIABLE_INLUCDE_experimental
         _point _equation_1(float x, float y, float xn=11, float yn=65) {
-            return _point((float f1(*(x)))*(xn), (float f2(*(y)))*(yn));
+            return _point((float _f1(*(x)))*(xn), (float _f2(*(y)))*(yn));
         }
 
         _point intersect(_line l1, _line_formula l2) {
