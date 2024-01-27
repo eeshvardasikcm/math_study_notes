d0eb2f13168c0017745c644d34eb5bc3949bb374
Eeshvar Das (Erik Douglas Ward) <contactme@eeshvardasikcm.com>
2024-01-27 01:54:52 -0800

    patch looks good

--- main.cpp
+++ main.cpp
@@ -30,17 +30,12 @@ namespace class10Od {
 
     struct unitOfMeasure {
     private:
-        std::char_traits<char> ;
     public:
         static const std::string km;
         static const std::string hr;
         static std::string kmPerH() {
-            std::string s=std::string(" ")
-                    .append(km)
-                    .append("/")
-                    .append(unitOfMeasure::hr)
-                    .append(" ");
-            return s;
+            std::basic_string bs = " " + km + "/" + unitOfMeasure::hr + " ";
+            return bs;
         };
     };
     const std::string unitOfMeasure::km = "km";
